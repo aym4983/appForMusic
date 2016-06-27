aaa82d11c82ff4ef42134c1f1f89b7dc245446ef
aym4983 <loic.chaucheprat@outlook.com>
2016-06-27 20:52:42 +0200

    Exemple de tests unitaires en TDD avec EasyMock

--- src/main/java/fr/imie/appformusic/controller/MainController.java
+++ src/main/java/fr/imie/appformusic/controller/MainController.java
@@ -1,15 +1,32 @@
 package fr.imie.appformusic.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import fr.imie.appformusic.domaine.User;
+import fr.imie.appformusic.service.UserMetier;
+
 @Controller
 public class MainController {
 
+	@Autowired
+	private UserMetier service;
+	
 	@RequestMapping("/main")
 	public ModelAndView init(){
 		return new ModelAndView("main");
 	}
+	
+	/*
+	 * Méthode d'exemple pour tests unitaire
+	 */
+	@RequestMapping("/main/all")
+	public List<User> getAllUsers(){
+		return service.findAllUsers();
+ 	}
 	
 }
--- src/test/java/fr/imie/appformusic/controller/MainControllerTest.java
+++ src/test/java/fr/imie/appformusic/controller/MainControllerTest.java
@@ -1,17 +1,71 @@
 package fr.imie.appformusic.controller;
 
-import org.junit.Test;
-import org.springframework.web.servlet.ModelAndView;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
+
+import org.easymock.EasyMockRunner;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.web.servlet.ModelAndView;
+
+import fr.imie.appformusic.domaine.User;
+import fr.imie.appformusic.service.UserMetier;
+
+@RunWith(EasyMockRunner.class)
 public class MainControllerTest {
 
+	@Mock
+	private UserMetier serviceMock;
+	
+	@TestSubject
 	private MainController controller = new MainController();
 	
 	@Test
 	public void testInit(){
 		ModelAndView path = controller.init();
 		assertEquals("main", path.getViewName());
+	}
+
+	@Test
+	public void testGetAllUsersOk() throws Exception {
+		List<User> listUsers = new ArrayList<>();
+		User user = new User();
+		user.setId(1);
+		user.setName("userTest");
+		user.setEmail("userTest");
+		listUsers.add(user);
+		
+		// Définition du comportement du mock
+		expect(serviceMock.findAllUsers()).andReturn(listUsers);
+		replay(serviceMock);
+		
+		// Appel de la méthode à tester
+		List<User>resultList = controller.getAllUsers();
+		
+		// assertions
+		assertEquals(resultList.size(), 1);
+		assertEquals(resultList.get(0).getName(), "userTest");
+	}
+	
+	/*
+	 * Si la méthode du service lance une exception
+	 */
+	@Test(expected=Exception.class)
+	public void testGetAllUsersKo(){
+		expect(serviceMock.findAllUsers()).andThrow(new Exception("Erreur")).times(1);
+		replay(serviceMock);
+		
+		// Appel de la méthode à tester
+		controller.getAllUsers();
+		
+		// Permet de controler que l'appel du mock à bien eu lieu (sinon exception)
+		verify(serviceMock.findAllUsers());
 	}
 	
 }

2a3f63c9c9009328c84baf8196b295b9011c3bc9
Dorian Herlory <dorian.herlory@gmail.com>
2016-07-28 16:26:44 +0200

    Suppression des imports inutiles de AppUser

--- src/main/java/fr/imie/appformusic/domain/AppUser.java
+++ src/main/java/fr/imie/appformusic/domain/AppUser.java
@@ -6,7 +6,6 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 /**
  * Classe représentant un utilisateur.

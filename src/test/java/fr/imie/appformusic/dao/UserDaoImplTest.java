fa93f70993d3e877432dc9f4443552f50d16e111
Dorian Herlory <dorian.herlory@gmail.com>
2016-07-28 11:06:04 +0200

    Modification des tests de UserDaoImpl

--- src/test/java/fr/imie/appformusic/dao/UserDaoImplTest.java
+++ src/test/java/fr/imie/appformusic/dao/UserDaoImplTest.java
@@ -1,13 +1,7 @@
 package fr.imie.appformusic.dao;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.List;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
@@ -16,11 +10,8 @@ import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import fr.imie.appformusic.configuration.TestDispatcherConfig;
-import fr.imie.appformusic.domain.Place;
-import fr.imie.appformusic.domain.AppUser;
 
 /**
  * 
@@ -44,27 +35,6 @@ public class UserDaoImplTest {
 				.setType(EmbeddedDatabaseType.HSQL)
 				.setName("test_base")
 				.build();
-	}
-	
-	@Test
-	@Ignore
-	@Transactional
-	public void testFindAllUsers(){
-		List<AppUser> listUser = userDao.findAllUsers();
-		assertThat(listUser).isNotNull();
-		assertThat(listUser.get(0).getUserName()).isEqualTo("test");
-		assertThat(listUser.get(0).getPlace().getIdPlace()).isEqualTo(2);
-	}
-	
-	@Test
-	@Transactional
-	public void testCreateUser(){
-		AppUser user = new AppUser(2, "user", "usermail", "mdp", 5, "loic", "chaucheprat");
-		Place place = new Place(1, "placename", "blablabla");
-		user.setPlace(place);
-		place.setUser(user);
-		
-		userDao.create(user);
 	}
 
 	@After

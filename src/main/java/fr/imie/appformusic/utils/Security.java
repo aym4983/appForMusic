7e845ca5d70c57c4b2e938ec1f3fcc7eb6d33ee5
Dorian <dorian.herlory@gmail.com>
2016-08-09 02:44:39 +0200

    Modification de la classe utils.Security.

--- src/main/java/fr/imie/appformusic/utils/Security.java
+++ src/main/java/fr/imie/appformusic/utils/Security.java
@@ -2,15 +2,17 @@ package fr.imie.appformusic.utils;
 
 import java.security.MessageDigest;
 
+import javax.xml.bind.DatatypeConverter;
+
 import fr.imie.appformusic.exceptions.TechnicalException;
 
 public class Security {
 	
 	public static String hashPassword(String password, String salt) {
 		try {
+			String toHash = password + salt;
 			MessageDigest md = MessageDigest.getInstance("SHA-512");
-			md.update(salt.getBytes("UTF-8"));
-			return md.digest(password.getBytes("UTF-8")).toString();
+			return DatatypeConverter.printHexBinary(md.digest(toHash.getBytes("UTF-8")));
 		} catch (Exception e) {
 			throw new TechnicalException(e);
 		}

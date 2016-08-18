407b52b53515a19a2e8b6a62b09a8f758c91eb58
TiphanieA <aubrytiphanie@gmail.com>
2016-08-18 14:40:16 +0200

    role

--- src/main/java/fr/imie/appformusic/domain/Role.java
+++ src/main/java/fr/imie/appformusic/domain/Role.java
@@ -14,8 +14,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
@@ -34,7 +32,6 @@ public class Role implements Serializable {
 	private String label;
 	
 	@ManyToMany(mappedBy = "roles")
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"),inverseJoinColumns = @JoinColumn(name = "username", referencedColumnName = "username"))
 	private Set<AppUser> users;
 
 	

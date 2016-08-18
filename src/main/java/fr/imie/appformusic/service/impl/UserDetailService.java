1cc8db498c06cf644fe0eb2bdc4fb8538d789146
aym4983 <loic.chaucheprat@outlook.com>
2016-08-18 10:00:37 +0200

    implémentation service

--- src/main/java/fr/imie/appformusic/service/impl/UserDetailService.java
+++ src/main/java/fr/imie/appformusic/service/impl/UserDetailService.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -27,9 +29,11 @@ public class UserDetailService implements UserDetailsService {
 		AppUser appUser = userDao.findByUserName(username);
 		
 		Set<GrantedAuthority> grantedAuthority = new HashSet<>();
-		for (Role role : appUser.getRoles())
+		for (Role role : appUser.getRoles()){
+			grantedAuthority.add(new SimpleGrantedAuthority(role.getLabel()));
+		}
 		
-		return null;
+		return new User(appUser.getUsername(), appUser.getPasswordHash(), grantedAuthority);
 	}
 
 }

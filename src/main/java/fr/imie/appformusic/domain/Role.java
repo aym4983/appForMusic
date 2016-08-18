213cec1a29e4b1b993461f970c80f399f1a9e824
TiphanieA <aubrytiphanie@gmail.com>
2016-08-18 14:14:07 +0200

    ROLE

--- src/main/java/fr/imie/appformusic/domain/Role.java
+++ src/main/java/fr/imie/appformusic/domain/Role.java
@@ -7,7 +7,6 @@
 package fr.imie.appformusic.domain;
 
 import java.io.Serializable;
-import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -15,6 +14,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;

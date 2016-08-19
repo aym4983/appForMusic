0171dfebf3760312fb681e3788ee77ab766344d1
Dorian <dorian.herlory@gmail.com>
2016-08-19 10:01:59 +0200

    Correstion du script import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,7 +2,6 @@ insert into role (label) values ('user');
 
 -- Tests
 
-insert into appuser (username, email, enabled, firstname, lastname, passwordhash)
-	values ('Test', 'test@test.fr', true, 'Test', 'Test', '$2a$10$WWQLKlimcQS4Cd4.FVcZ7eJVhJFMdTTrA7Zae85wUzOhUYi073CbW');
+insert into appuser (username, email, enabled, firstname, lastname, passwordhash) values ('test', 'test@test.fr', true, 'Test', 'Test', '$2a$10$WWQLKlimcQS4Cd4.FVcZ7eJVhJFMdTTrA7Zae85wUzOhUYi073CbW');
 	
 insert into user_role (username, role_id) values ('Test', 1);

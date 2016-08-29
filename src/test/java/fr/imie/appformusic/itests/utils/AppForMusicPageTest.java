bb07b016e7f0110212ff29188131dfc933a4e2f9
aym4983 <loic.chaucheprat@gmail.com>
2016-08-29 15:20:11 +0200

    Update AppForMusicPageTest.java

--- src/test/java/fr/imie/appformusic/itests/utils/AppForMusicPageTest.java
+++ src/test/java/fr/imie/appformusic/itests/utils/AppForMusicPageTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ActiveProfiles;
 
 import fr.imie.appformusic.itests.pages.HomePage;
 

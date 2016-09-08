599e971b24f48b9231fcc9407db2e820038a737a
Lila <chevalier.lila@gmail.com>
2016-09-08 20:22:02 +0200

    correction sql commentaire upstream

--- ajoutdonnees.sql
+++ ajoutdonnees.sql
@@ -1,8 +1,6 @@
 
-<<<<<<< HEAD
 INSERT INTO role(id, label) VALUES (1, 'admin');
 INSERT INTO role(id, label) VALUES (2, 'utilisateur');
-=======
+
 INSERT INTO role(id, label) VALUES (1, admin);
 INSERT INTO role(id, label) VALUES (2, utilisateur);
->>>>>>> refs/remotes/origin/master_tiphanie
